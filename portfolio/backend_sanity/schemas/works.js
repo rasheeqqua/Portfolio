5c8819b29e5812dbf41d5301c3784ba0e0544e76
rasheeqqua <201728007@student.mist.ac.bd>
2022-08-03 23:02:36 +0600

    Add files via upload
    added the sanity schemas for setting up back-end

---
+++ portfolio/backend_sanity/schemas/works.js
@@ -0,0 +1,67 @@
+export default {
+    name: 'works',
+    title: 'Works',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+    
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+      {
+        name: 'projectLink',
+        title: 'Project Link',
+        type: 'string',
+      },
+      {
+        name: 'codeLink',
+        title: 'Code Link',
+        type: 'string',
+      },
+      {
+        name: 'imgUrl',
+        title: 'ImageUrl',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+   
+      {
+        name: 'tags',
+        title: 'Tags',
+       type:'array',
+       of: [
+         {
+           name:'tag',
+           title:'Tag',
+           type:'string'
+         }
+       ]
+      },
+     
+    ],
+    orderings: [
+      {
+        title: 'Description, Brief',
+        name: 'descriptionAsc',
+        by: [
+          {field: 'description', direction: 'asc'}
+        ]
+      },
+      {
+        title: 'Description, Extended',
+        name: 'descriptionDesc',
+        by: [
+          {field: 'description', direction: 'desc'}
+        ]
+      },
+
+    ]
+  };
